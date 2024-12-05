3b86f4184cd3408020a589260351cda28c457039
VanillaMile <94708909+VanillaMile@users.noreply.github.com>
2024-12-05 13:58:29 +0100

    Updates to basic files

--- application/__init__.py
+++ application/__init__.py
@@ -1,4 +1,11 @@
 from flask import Flask
+from flask_pymongo import PyMongo
+
 app = Flask(__name__)
+app.config["MONGO_URI"] = "mongodb://localhost:27017/dcaDatabase"
+
+mongo = PyMongo(app)
+client = mongo.db
+stocks = client.stocks
 
 from application import routes
--- application/routes.py
+++ application/routes.py
@@ -1,5 +1,11 @@
 from application import app
+from flask import render_template
+from application import *
 
 @app.route("/")
 def index():
-    return "Hello, world!"
+    return render_template("layout.html", title = "DCASimulator")
+
+@app.route("/stocks")
+def stocks():
+    return render_template("stocks.html", title = "Stocks")
