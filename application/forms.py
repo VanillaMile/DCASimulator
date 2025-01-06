ce342beded1e052a9302acad33f40cb251cd5e3a
VanillaMile <94708909+VanillaMile@users.noreply.github.com>
2025-01-06 17:08:13 +0100

    Rankings
    Added ranking and cleaned up some code

--- application/forms.py
+++ application/forms.py
@@ -6,3 +6,71 @@ def getStocks():
     for stock in stocks:
         stocksNames.append(stock["stock_name"])
     return stocksNames
+
+def insertRankingResult(userName, stockName, initialInvestment, investmentAmount, investmentInterval, investmentPeriod, profit, profitPercentage, input):
+    if mongo.db.rankings.find_one({"username": userName}) is None:
+        mongo.db.rankings.insert_one({
+            "username": userName,
+            "stock_name": stockName,
+            "initial_investment": float(initialInvestment),
+            "investment_amount": float(investmentAmount),
+            "investment_interval": float(investmentInterval),
+            "investment_period": float(investmentPeriod),
+            "profit": float(profit),
+            "profit_percentage": float(profitPercentage),
+            "input": float(input)})
+    else:
+        mongo.db.rankings.update_one({"user_name": userName}, 
+                         {"$set": {
+                        "stockname": stockName,
+                        "initial_investment": float(initialInvestment),
+                        "investment_amount": float(investmentAmount), 
+                        "investment_interval": float(investmentInterval),
+                        "investment_period": float(investmentPeriod),
+                        "profit": float(profit),
+                        "profit_percentage": float(profitPercentage),
+                        "input": float(input)}})
+
+def getSortedRankingData():
+    pipeline = [
+        {
+            "$group": {
+                "_id": "$username",
+                "profit": {"$sum": "$profit"},
+                "percentage_grow": {"$avg": "$profit_percentage"},
+                "period": {"$avg": "$investment_period"},
+                "stock_name": {"$first": "$stock_name"},
+                "investment_amount": {"$first": "$investment_amount"},
+                "initial_investment": {"$first": "$initial_investment"},
+                "investment_interval": {"$first": "$investment_interval"},
+                "input": {"$first": "$input"}
+            }
+        },
+        {
+            "$addFields": {
+                "score": {
+                    "$cond": {
+                        "if": {"$eq": ["$period", 0]},
+                        "then": 0,
+                        "else": {"$divide": ["$percentage_grow", "$period"]}
+                    }
+                }
+            }
+        },
+        {
+            "$sort": {"score": -1}
+        },
+        {
+            "$limit": 10
+        }
+    ]
+    results = list(mongo.db.rankings.aggregate(pipeline))
+
+    # print(results)
+    # for row in results:
+    #     print(f"Username: {row['_id']}")
+    #     print(f"Profit: {row['profit']}")
+    #     print(f"Percentage Grow: {row['percentage_grow']}")
+    #     print(f"Period: {row['period']}")
+    #     print(f"Score: {row['score']}")
+    return results
--- application/routes.py
+++ application/routes.py
@@ -3,7 +3,6 @@ from flask import render_template, request
 from application.forms import *
 from application.simulationValidation import simulationValidation
 from application.simulation import simulation
-import json
 
 @app.route("/")
 def index():
@@ -30,14 +29,34 @@ def simulationResults():
         "investment_period": request.form.get("investment_period"),
         "stock": request.form.get("stock"),
         "stock_data": mongo.db.stocks.find_one({"stock_name": request.form.get("stock")}),
-        "predict_days": request.form.get("predict_days")
     }
 
+    # Validation
     returnStatus = simulationValidation(simulationData['initial_investment'], simulationData['investment_amount'], simulationData['investment_interval'], 
                                         simulationData['investment_period'], simulationData['stock_data'])
-
     if returnStatus != [None, None, None, None, None]:
         return render_template("simulationForm.html",   title = "Simulation Form", stock_names = getStocks(), return_status = returnStatus)
     
+    # Simulation
     data = simulation(simulationData)
-    return render_template("simulation.html",   title = "Simulation", investment_data = data, json_data = json.dumps(data))
+    return render_template("simulation.html",   title = "Simulation", investment_data = data)
+
+@app.route("/submitRankingSubmit", methods = ["POST"])
+def submitRankingSubmit():
+    userName = request.form.get("username")
+    stockName = request.form.get("stock_name")
+    initialInvestment = request.form.get("initial_investment")
+    investmentAmount = request.form.get("investment_amount")
+    investmentInterval = request.form.get("investment_interval")
+    investmentPeriod = request.form.get("investment_period")
+    input = request.form.get("input")
+    profit = request.form.get("profit")
+    profitPercentage = request.form.get("profit_percent")
+    insertRankingResult(userName, stockName, initialInvestment, investmentAmount, investmentInterval, investmentPeriod, profit, profitPercentage, input)
+    rankingData = getSortedRankingData()
+    return render_template("ranking.html", title = "Ranking", ranking_data = rankingData)
+
+@app.route("/seeRanking")
+def seeRanking():
+    rankingData = getSortedRankingData()
+    return render_template("ranking.html", title = "Ranking", ranking_data = rankingData)
